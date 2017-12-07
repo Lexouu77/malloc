1b48cd2849a34576eda8bae36c964402539d786e
Alexis Hamouda <ahamouda@student.42.fr>
2017-12-07 20:28:33 +0100

    [Malloc] Fixed a failing free for unmapped LARGE.

--- realloc.c
+++ realloc.c
@@ -6,11 +6,17 @@
 /*   By: ahamouda <ahamouda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/29 15:35:35 by ahamouda          #+#    #+#             */
-/*   Updated: 2017/12/06 21:00:52 by ahamouda         ###   ########.fr       */
+/*   Updated: 2017/12/07 20:27:14 by ahamouda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "malloc.h"
+
+static size_t	is_mapped(void *ptr)
+{
+
+	return (0);
+}
 
 void	*realloc(void *ptr, size_t size)
 {
@@ -19,8 +25,9 @@ void	*realloc(void *ptr, size_t size)
 	
 	if (!ptr || !size)
 		return (NULL);
+
 	if (is_remappable(ptr, aligned_size, type))
 		return (remap_block(ptr, aligned_size, type));
 	free(ptr);
-	return (malloc(ptr, aligned_size));
+	return (malloc(aligned_size));
 }
