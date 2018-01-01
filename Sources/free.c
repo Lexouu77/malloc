8d741e829586f059fffe5d3eda6d0862a6751ebe
Lexouu77 <ahamouda@student.42.fr>
2018-01-01 18:55:21 +0100

    [Malloc] Removed useless function in free

--- Sources/free.c
+++ Sources/free.c
@@ -6,7 +6,7 @@
 /*   By: ahamouda <ahamouda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/06 20:02:03 by ahamouda          #+#    #+#             */
-/*   Updated: 2018/01/01 18:38:18 by ahamouda         ###   ########.fr       */
+/*   Updated: 2018/01/01 18:43:36 by ahamouda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,6 @@ void				group_pages(t_block *block)
 		ptr->size += ptr->next->size + SZ_PAGE;
 		ptr->next = ptr->next->next;
 	}
-}
-
-static size_t		is_unmappable(t_block *block)
-{
-	if (!block->pages)
-		return (1);
-	return (0);
 }
 
 static size_t		set_available(t_block *block, void *ptr)
@@ -98,11 +91,8 @@ void				free(void *ptr)
 		pthread_mutex_unlock(&g_m_mutex);
 		return ;
 	}
-	if (is_unmappable(block))
-	{
-		if (!block->pages)
-			cut_link(block);
-	}
+	if (!block->pages)
+		cut_link(block);
 	else
 		group_pages(block);
 	pthread_mutex_unlock(&g_m_mutex);
